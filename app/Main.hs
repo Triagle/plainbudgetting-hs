9297e597bd5c78e8e9f7d511f468fb5790ca838f
Jake Faulkner <jakefaulkn@gmail.com>
2018-06-01 19:08:43 +1200

    :sparkles: start CLI implementation

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,30 @@ module Main where
 import Lib
 import Text.Parsec
 import Data.Either
+import Data.Semigroup ((<>))
+import Options.Applicative
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Csv as C
 
-processBudget :: String -> String
-processBudget = either show (printBudget . runBudget) . parse (readLine `sepBy` newline :: Parsec String () [BudgetLine Double]) ""
+data FormatType = CSV | PlainText
+
+data Options = Options {
+    input :: FormatType,
+    output :: FormatType
+}
+
+parseBudget :: String -> Either Text.Parsec.ParseError [BudgetLine Double]
+parseBudget = parse (readLine `sepBy` newline) ""
+
+processBudget :: FormatType -> FormatType -> String -> String
+processBudget i o = either show (outputFn o . runBudget) . (formatFn i)
+    where formatFn CSV = toList <$> C.decode C.NoHeader . B.pack
+          formatFn PlainText = parseBudget
+          outputFn CSV = B.unpack . C.encode
+          outputFn PlainText = printBudget
+ 
+optParse :: Parser Options
+optParse = Option <$> flag PlainText CSV (long "csv" <> help "Set input type to csv") <*> flag PlainText CSV (long "output-csv" <> help "Set output type to CSV")
 
 main :: IO ()
-main = interact processBudget
+main = exec  interact processBudget
