850dfc25e8e7f7bbaf3f4dd6a01d2bc4221c44de
Jake <jakefaulkn@gmail.com>
2018-06-02 19:10:28 +1200

    :sparkles: add CLI frontend

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,75 @@
 module Main where
 
 import Lib
-import Text.Parsec
+import qualified Text.Parsec as P
+import qualified Text.Parsec.Error as E
+import Data.List as L
+import Data.Char (toLower)
+import Data.Bifunctor
 import Data.Either
+import qualified Data.Vector as V
 import Data.Semigroup ((<>))
 import Options.Applicative
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Csv as C
 
-data FormatType = CSV | PlainText
+data FormatType = CSV | Plain deriving (Show)
+
+instance Read FormatType where
+    readsPrec _ str 
+        | "csv" `L.isPrefixOf` map toLower str = [(CSV, drop 3 str)]
+        | "plain" `L.isPrefixOf` map toLower str = [(Plain, drop 5 str)]
+        | otherwise = []
 
 data Options = Options {
     input :: FormatType,
     output :: FormatType
 }
 
-parseBudget :: String -> Either Text.Parsec.ParseError [BudgetLine Double]
-parseBudget = parse (readLine `sepBy` newline) ""
+parseBudget :: String -> Either P.ParseError [BudgetLine Double]
+parseBudget = P.parse (readLine `P.sepBy` P.newline) ""
 
 processBudget :: FormatType -> FormatType -> String -> String
-processBudget i o = either show (outputFn o . runBudget) . (formatFn i)
-    where formatFn CSV = toList <$> C.decode C.NoHeader . B.pack
-          formatFn PlainText = parseBudget
-          outputFn CSV = B.unpack . C.encode
-          outputFn PlainText = printBudget
- 
-optParse :: Parser Options
-optParse = Option <$> flag PlainText CSV (long "csv" <> help "Set input type to csv") <*> flag PlainText CSV (long "output-csv" <> help "Set output type to CSV")
+processBudget i o = either show (outputFn o . runBudget) . formatFn i
+ where
+  formatFn CSV = (>>= return . V.toList) . C.decode C.NoHeader . B.pack
+  formatFn Plain =
+    first (mconcat . map ((++) "\n" . E.messageString) . E.errorMessages)
+      . parseBudget
+  outputFn CSV       = B.unpack . C.encode
+  outputFn Plain = printBudget
+
+opts :: Parser Options
+opts =
+  Options
+    <$> option
+          auto
+          (  long "input"
+          <> short 'i'
+          <> help "Set input type"
+          <> value Plain
+          <> showDefault
+          <> metavar "TYPE"
+          )
+    <*> option
+          auto
+          (  long "output"
+          <> short 'o'
+          <> help "Set output type"
+          <> value Plain
+          <> showDefault
+          <> metavar "TYPE"
+          )
+
+pinfo :: ParserInfo Options
+pinfo = info
+  (opts <**> helper)
+  (  fullDesc
+  <> progDesc "Parse and manage a budget through plain text."
+  <> header "plainbudgetting - a budgetting system in plain text"
+  )
 
 main :: IO ()
-main = exec  interact processBudget
+main = do
+  opts <- execParser pinfo
+  interact $ processBudget (input opts) (output opts)
