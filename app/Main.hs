6513ebcaf261b8d911368ecaee496de641992397
Jake <jakefaulkn@gmail.com>
2018-06-03 23:22:17 +1200

    :sparkles: refactor to allow for scoped sums

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import qualified Text.Parsec as P
 import qualified Text.Parsec.Error as E
+import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
+import qualified Data.Text.Lazy.IO as TIO
 import Data.List as L
 import Data.Char (toLower)
 import Data.Bifunctor
@@ -10,13 +14,12 @@ import Data.Either
 import qualified Data.Vector as V
 import Data.Semigroup ((<>))
 import Options.Applicative
-import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Csv as C
 
 data FormatType = CSV | Plain deriving (Show)
 
 instance Read FormatType where
-    readsPrec _ str 
+    readsPrec _ str
         | "csv" `L.isPrefixOf` map toLower str = [(CSV, drop 3 str)]
         | "plain" `L.isPrefixOf` map toLower str = [(Plain, drop 5 str)]
         | otherwise = []
@@ -26,17 +29,18 @@ data Options = Options {
     output :: FormatType
 }
 
-parseBudget :: String -> Either P.ParseError [BudgetLine Double]
+parseBudget :: T.Text -> Either P.ParseError [BudgetLine Double]
 parseBudget = P.parse (readLine `P.sepBy` P.newline) ""
 
-processBudget :: FormatType -> FormatType -> String -> String
-processBudget i o = either show (outputFn o . runBudget) . formatFn i
+processBudget :: FormatType -> FormatType -> T.Text -> T.Text
+processBudget i o = either (T.pack . show) (outputFn o . runBudget)
+  . formatFn i
  where
-  formatFn CSV = (>>= return . V.toList) . C.decode C.NoHeader . B.pack
+  formatFn CSV = fmap V.toList . C.decode C.NoHeader . encodeUtf8
   formatFn Plain =
-    first (mconcat . map ((++) "\n" . E.messageString) . E.errorMessages)
+    first (mconcat . map ((<>) "\n" . E.messageString) . E.errorMessages)
       . parseBudget
-  outputFn CSV       = B.unpack . C.encode
+  outputFn CSV   = decodeUtf8 . C.encode
   outputFn Plain = printBudget
 
 opts :: Parser Options
@@ -72,4 +76,4 @@ pinfo = info
 main :: IO ()
 main = do
   opts <- execParser pinfo
-  interact $ processBudget (input opts) (output opts)
+  TIO.interact $ processBudget (input opts) (output opts)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     (lineItem,
     Flow,
@@ -11,14 +13,19 @@ module Lib
 import Text.Parsec
 import qualified Data.DList as DL
 import Control.Monad (void, msum, join, mzero)
+import Data.Monoid ((<>))
 import Data.Foldable (foldrM)
 import qualified Control.Monad.Trans.State as ST
+import qualified Control.Monad.Fail as MF
 import Data.Functor (($>))
 import Text.Parsec.Char
 import Data.Csv
+import Data.Either (either)
+import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Read (signed, rational)
 
 data Flow a = Flow {
-    flowName :: String,
+    flowName :: T.Text,
     flowValue :: a
 } deriving (Show, Eq, Ord)
 
@@ -33,78 +40,83 @@ instance Monad Flow where
     return = pure
     (>>=) flow f = f (flowValue flow)
 
-eol :: Parsec String () ()
+eol :: Parsec T.Text () ()
 eol = void (char '\n') <|> eof
 
 whitespace = oneOf " \t"
 
-readFlow :: Read a => Char -> String -> String -> Flow a
-readFlow '+' val name = Flow name (read val)
-readFlow _   val name = Flow name (read $ '-' : val)
+readFlow :: Char -> T.Text -> T.Text -> Either String (Flow Double)
+readFlow sign val name =
+  Flow name . fst <$> signed rational (T.singleton sign <> val)
 
-lineCount :: (Read a, Integral a) => Parsec String () a
-lineCount = whitespace *> char 'x' *> whitespace *> (read <$> many1 digit)
+eitherFail :: MF.MonadFail m => Either String b -> m b
+eitherFail = either MF.fail return
 
-flow :: Read a => Parsec String () (Flow a)
+lineCount :: Parsec T.Text () Double
+lineCount =
+  whitespace
+    *> char 'x'
+    *> whitespace
+    *> (many1 digit >>= eitherFail . fmap fst . signed rational . T.pack)
+
+flow :: Parsec T.Text () (Flow Double)
 flow = do
   sgn <- oneOf "+-"
   void whitespace
-  val <- many digit
+  val <- T.pack <$> many digit
   void whitespace
-  name <- manyTill anyChar (try . lookAhead $ void lineCount <|> eol)
-  return $ readFlow sgn val name
+  name <- T.pack <$> manyTill anyChar (try . lookAhead $ void lineCount <|> eol)
+  eitherFail $ readFlow sgn val name
 
-lineItem :: (Floating a, Read a) => Parsec String () (Flow a)
+lineItem :: Parsec T.Text () (Flow Double)
 lineItem = do
   fl    <- flow
-  count <- fromIntegral <$> (lineCount <|> return 1)
+  count <- lineCount <|> return 1
   return $ (* count) <$> fl
 
-data BudgetLine a = LineItem (Flow a) | Comment String | Sum a deriving (Show, Eq)
+data BudgetLine a = LineItem (Flow a) | Comment T.Text | Sum a deriving (Show, Eq)
 
 instance FromField a => FromRecord (BudgetLine a) where
-    parseRecord v 
+    parseRecord v
         | length v < 1 = mzero
-        | otherwise = v .! 0 >>= (\name -> case name of
+        | otherwise = (v .! 0 :: Parser T.Text) >>= (\case
             "Flow" -> LineItem <$> (Flow <$> v .! 1 <*> v .! 2)
             "Comment" -> Comment <$> v .! 1
             "Sum" -> Sum <$> v .! 1
             _ -> mzero)
 
 instance ToField a => ToRecord (BudgetLine a) where
-    toRecord (Sum v) = record [toField "Sum", toField v]
-    toRecord (LineItem f) = record [toField "Flow", (toField . flowName) f, (toField . flowValue) f]
-    toRecord (Comment s) = record [toField "Comment", toField s]
+    toRecord (Sum v) = record [toField ("Sum" :: T.Text), toField v]
+    toRecord (LineItem f) = record [toField ("Flow" :: T.Text), (toField . flowName) f, (toField . flowValue) f]
+    toRecord (Comment s) = record [toField ("Comment" :: T.Text), toField s]
 
-sectionSum :: Num a => Parsec String () (BudgetLine a)
-sectionSum = char '=' $> Sum 0 <* optional (spaces *> manyTill anyChar (try . lookAhead $ eol))
+sectionSum :: Num a => Parsec T.Text () (BudgetLine a)
+sectionSum = char '=' $> Sum 0 <* manyTill (whitespace <|> digit) (try . lookAhead $ eol)
 
-comment :: Parsec String () (BudgetLine a)
-comment = Comment <$> manyTill anyChar (try . lookAhead $ eol)
+comment :: Parsec T.Text () (BudgetLine a)
+comment = Comment <$> (T.pack <$> manyTill anyChar (try . lookAhead $ eol))
 
-budgetLine :: (Floating a, Read a) => Parsec String () (BudgetLine a)
+budgetLine :: Parsec T.Text () (BudgetLine Double)
 budgetLine = LineItem <$> lineItem
 
-readLine :: (Floating a, Read a) => Parsec String () (BudgetLine a)
-readLine = try budgetLine <|> sectionSum <|> comment
+readLine :: Parsec T.Text () (BudgetLine Double)
+readLine = try budgetLine <|> try sectionSum <|> comment
 
-runBudgetLine :: Floating a => BudgetLine a -> ST.State [Flow a] (BudgetLine a)
-runBudgetLine (Comment s) = return $ Comment s
-runBudgetLine (LineItem f) = ST.modify (\bs -> f:bs) >> return (LineItem f)
-runBudgetLine (Sum v) = do 
-    stck <- ST.get
-    ST.put []
-    return . Sum . sum . map flowValue $ stck
+runBudgetLine :: Num a => BudgetLine a -> ST.State [Flow a] (BudgetLine a)
+runBudgetLine (Comment "") = ST.put [] >> return (Comment "")
+runBudgetLine (Comment  s) = return $ Comment s
+runBudgetLine (LineItem f) = ST.modify (\bs -> f : bs) >> return (LineItem f)
+runBudgetLine (Sum      v) = Sum . sum . map flowValue <$> ST.get 
 
-runBudget :: Floating a => [BudgetLine a] -> [BudgetLine a]
+runBudget :: (Floating a, Show a) => [BudgetLine a] -> [BudgetLine a]
 runBudget lines = ST.evalState (mapM runBudgetLine lines) []
-  
-printBudget :: (Ord a, Floating a, Show a) => [BudgetLine a] -> String
-printBudget = unlines . map printLine
+
+printBudget :: (Ord a, Floating a, Show a) => [BudgetLine a] -> T.Text
+printBudget = T.unlines . map printLine
  where
   printLine (Comment s) = s
   printLine (LineItem (Flow name val)) =
-    sign val ++ " " ++ (show . abs) val ++ " " ++ name
-  printLine (Sum v) = "= " ++ show v
+    sign val <> " " <> (T.pack . show . abs) val <> " " <> name
+  printLine (Sum v) = "= " <> (T.pack . show) v
   sign v | v >= 0    = "+"
          | otherwise = "-"
